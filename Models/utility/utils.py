1a7dd1a78b704b9fac14ad58d2ab030a0316ae23
Olagoke <adderbyte@users.noreply.github.com>
2020-01-20 17:55:48 +0300

    Add files via upload

--- Models/utility/utils.py
+++ Models/utility/utils.py
@@ -1,5 +1,8 @@
 
 import matplotlib.pyplot as plt
+import os 
+import pandas as pd
+import tensorflow as tf
 
 def plot_meter(train, leak, start=0, n=100, bn=10,n_plots=5):
     '''
@@ -23,5 +26,58 @@ def plot_meter(train, leak, start=0, n=100, bn=10,n_plots=5):
         plt.show()
         if count > n_plots:
             break
-        
+   
+
+def tfdatabuilder(cleanedpath,cleandataName,trainName, validateName, validation_split ):
+    
+    # get data set
+    train_test = pd.read_csv(cleanedpath +  cleandataName )
+    cleandataName# set index to timestamp
+    train_test.set_index('timestamp',inplace=True);
+    # del colum unnmaed
+    #del train['Unnamed: 0'];
+    # get column names
+    #print(train_test.columns)
+    col_names = list(train_test.columns)#[1:]
+    print(col_names)
+    # save the data types 
+    data_types = list(train_test.dtypes)#[1:]
+    # convert the data types to 
+    # tensorflow data types 
+    for i,j in enumerate(data_types):
+        j = str(j) # convertr from numpy dtype to str
+        #print(j)
+        if j == 'float64':
+            data_types[i] = tf.float64
+        else:
+            data_types[i]  = tf.int32
+    
+    print(data_types)
+    
+    # separate train and validation
+    test = train_test.tail(validation_split).copy(deep=True)
+    train_= train_test.shape[0]-test.shape[0] # train partition
+    train = train_test[:train_].copy(deep=True)
+    
+    # savedata
+    train.to_csv(cleanedpath + trainName,index=False,header=None)
+    test.to_csv(cleanedpath + validateName,index=False,header=None)
+    print('====================================')
+    print('Done')
+    
+    return train.index.values,test.index.values,data_types, col_names
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         
